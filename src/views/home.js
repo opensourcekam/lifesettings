5d930fbdc8153148e1c8d5c3b75145926632a9a5
Kameron Robinson <kameronrobinson@Kamerons-MacBook.local>
2018-06-06 00:51:52 +0100

    add credits

--- src/views/home.js
+++ src/views/home.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Field, reduxForm } from 'redux-form';
 import { Toggle } from '../components';
 import settings from '../constants/settings';
+import {createEventHandlerWithConfig} from 'recompose'
 
 const Container = styled.div`
   margin-top: 4rem;
@@ -23,6 +24,17 @@ const SettingWrapper = styled.div`
   margin-bottom: 13px;
 `;
 
+const Credit = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  color: ${({ theme }) => theme.colors.grey4};
+  p {
+    font-size: 0.8rem
+    margin: 0;
+  }
+`;
+
 const Home = () => (
   <Container>
     <Form>
@@ -37,6 +49,10 @@ const Home = () => (
         </SettingWrapper>
       ))}
     </Form>
+    <Credit>
+      <p>Designed by Robert Gallardo - @robertgallardo</p>
+      <p>Coded by Kameron Robinson - @kam.ofthefuture</p>
+    </Credit>
   </Container>
 )
 
